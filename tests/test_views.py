d71d2f999b7aa6abcf4edfcd5b42ba5ce1c91cbd
Paul Carver <dbdkmezz@gmail.com>
2017-12-18 07:41:37 +0000

    test with less mocks, test all the way down

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,36 +1,26 @@
-from unittest.mock import Mock, patch
+import pytest
 
 from django.test import TestCase
 from django.http import HttpResponse
 
 from libs.google_actions.tests.mocks import MockRequest
+from libs.google_actions.tests.utils import Utils as GoogleTestUtils
 
 from apps.word_finding.views import index
 
+from .factories import QuestionFactory
 
+
+@pytest.mark.django_db
 class TestViews(TestCase):
     def test_hello_world_if_not_json(self):
         result = index(MockRequest(body='NOT JSON'))
         self.assertIsInstance(result, HttpResponse)
         self.assertIn("Hello world", str(result.content))
 
-    @patch('apps.word_finding.views.JsonResponse')
-    @patch('apps.word_finding.views.AppRequest')
-    @patch('apps.word_finding.views.User.objects.get_or_create')
-    @patch('apps.word_finding.views.AppResponse')
-    def test_welcome_if_new_user(
-            self,
-            mock_app_response,
-            mock_get_or_create_user,
-            *args
-    ):
-        mock_user = Mock()
-        mock_user.get_next_question.return_value = "What's a pea?"
-        mock_get_or_create_user.return_value = (mock_user, True)
-
-        index(None)
-        ask_string = mock_app_response().ask.call_args[0][0]
-
-        self.assertEqual(mock_app_response().ask.call_count, 1)
-        self.assertIn('Welcome to word finding practice. ', ask_string)
-        self.assertIn(" What's a pea?", ask_string)
+    def test_welcome_if_new_user(self):
+        QuestionFactory(question="What's a pea?")
+        response = index(MockRequest())
+        response_text = GoogleTestUtils.get_text_from_google_response(response)
+        self.assertIn('Welcome to word finding practice. ', response_text)
+        self.assertIn(" What's a pea?", response_text)
